94fbcb305d5e6fcc34fc8c22aa6fdfdbb0e3dbb3
Stuart Davidson <stuartd96@live.co.uk>
2018-06-30 18:12:55 +0100

    Initial blog page layout

---
+++ resources/views/main-site/blog.blade.php
@@ -0,0 +1,30 @@
+
+@extends('layouts.main')
+
+@section('content')
+    <div class="row justify-content-center page-header" id="blog">
+        <div class="col-md-12 my-auto text-white">
+            <div class="container">
+                <h2>LATEST POSTS</h2>
+            </div>
+        </div>
+    </div>
+
+    <div class="bg-white">
+        <div class="container main">
+
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Special title treatment</h5>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
+@endsection
+
+
+ 
