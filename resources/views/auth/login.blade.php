f19bc4410c83cd3069c4530e5c8a207ac63b260b
Stuart Davidson <stuartd96@live.co.uk>
2018-06-30 18:12:19 +0100

    Added new header banners to cards

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <div class="card-header text-white text-uppercase font-weight-bold bg-dark">{{ __('Login') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}" aria-label="{{ __('Login') }}">
