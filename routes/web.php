d341d5df7da09be7df4d8b92d910fe2631c33aea
Stuart Davidson <stuartd96@live.co.uk>
2018-06-30 18:12:28 +0100

    Added initial blog routes

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,6 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 
 Route::get('/', function () {
     return view('main-site.main');
@@ -20,6 +11,14 @@ Route::get('/', function () {
  */
 Route::prefix('dashboard')->group(function(){
     Route::get('/', 'HomeController@index')->name('home');
+});
+
+/**
+ * Blog Routes
+ */
+Route::prefix('blog')->group(function(){
+    Route::get('/', 'BlogController@index')->name('blog');
+    Route::get('/{slug}', 'BlogController@read');
 });
 
 /**
